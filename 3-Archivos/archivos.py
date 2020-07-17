2418f124d6223834c9bef9cdcea5f0f2607de41c
erioni9512 <erickadrielnet@gmail.com>
2020-07-16 23:09:32 -0300

    evaluacion

--- 3-Archivos/archivos.py
+++ 3-Archivos/archivos.py
@@ -41,3 +41,27 @@ archivo = open("ArchivoDeTexto.txt", "a")
 
 archivo.write("\nAsi agrego esta linea")
 """
+
+"""
+def sumaDigitos(numero):
+    suma = 0
+    while numero != 0:
+        digito = numero % 10
+        suma = suma+digito
+        numero = numero//10
+    return suma
+
+
+sumatoria = 0
+num = int(input("Número a procesar: "))
+while num != 0:
+    print("Suma:", sumaDigitos(num))
+    sumatoria = sumatoria+num
+    num = int(input("Número a procesar: "))
+print("Sumatoria:", sumatoria)
+print("Dígitos:", sumaDigitos(sumatoria))
+"""
+
+
+while a in range(0, 4):
+    print("asd")
